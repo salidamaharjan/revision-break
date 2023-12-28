4eaa7f7e155ba5e1676879010d28fe27f7891e83
salidamaharjan <salidamaharjan@outlook.com>
2023-12-27 23:00:09 -0600

    Hangman revised code

--- HANGMAN/script.js
+++ HANGMAN/script.js
@@ -4,73 +4,57 @@ const retryBtn = document.querySelector(".btn-retry");
 let word = document.querySelector(".guess");
 let leftWord = document.querySelector(".word-num");
 let scoreNum = document.querySelector(".score-num");
+let randomWord;
 
-leftWord.textContent = 5;
-scoreNum.textContent = 0;
-let count = 0;
-
+startBtn.addEventListener("click", hangman);
 nextBtn.setAttribute("style", "display: none");
 retryBtn.setAttribute("style", "display: none");
-nextBtn.addEventListener("click", hangman);
-retryBtn.addEventListener("click", () => {location.reload()});
 
-word.setAttribute("style", "display: none");
-startBtn.addEventListener("click", hangman);
 function hangman() {
-  word.removeAttribute("style");
-  let words = ["PROGRAMMING", "INVESTMENT", "DILIGENCE", "PATIENCE", "SUCCESS"];
-  let enteredLetter = [];
-  let ranWord = words[randomNum(words.length)].toLowerCase();
-  console.log(ranWord);
+  displayWord();
+  nextBtn.addEventListener("click", displayWord);
+}
+document.addEventListener("keydown", showCorrectLetter);
+
+function showCorrectLetter(e) {
+  console.log(randomWord);
+  if (randomWord.includes(e.key)) {
+    // console.log("pressed key: ", e.key);
+    randomWord.split("").forEach((element, index) => {
+      if (element === e.key) {
+        const letterToDisplay = word.textContent.split("");
+        letterToDisplay.splice(index, 1, e.key.toUpperCase());
+        // console.log(`index of ${e.key}: `,index);
+        word.textContent = letterToDisplay.join("");
+        // console.log(word.textContent);
+        if (word.textContent.toLowerCase() === randomWord) {
+          alert("You got it");
+          nextBtn.removeAttribute("style");
+        }
+        // console.log("after replace: ", word.textContent);
+      }
+    });
+  } else {
+    alert("Incorrect Letter");
+  }
+}
+
+function displayWord() {
+  const wordList = [
+    "patience",
+    "diligent",
+    "investment",
+    "programmer",
+    "success",
+  ];
+  randomWord = wordList[randomNum(wordList.length)];
+  console.log(randomWord);
   word.textContent = "";
-  for (let i = 0; i < ranWord.length; i++) {
+  for (let i = 0; i < randomWord.length; i++) {
     word.textContent += "_";
   }
-  //   console.log(word.textContent.length);
-  document.addEventListener("keydown", (e) => {
-    // console.log("before if", e.key);
-    // console.log(ranWord.includes(e.key));
-    if (ranWord.includes(e.key)) {
-      //   console.log(ranWord.indexOf(e.key));
-      let i = -1;
-      let indexes = [];
-      while (((i = ranWord.indexOf(e.key, i + 1)), i !== -1)) {
-        // console.log("while loop", ranWord.indexOf(e.key));
-        indexes.push(i);
-        if (!enteredLetter.includes(e.key)) {
-          enteredLetter.push(ranWord[i]);
-          count += 1;
-          scoreNum.textContent = count;
-        }
-      }
-      console.log(enteredLetter);
-      console.log("count", count);
-
-      let guessWord = word.textContent.split("");
-      console.log(guessWord.length);
-      indexes.forEach((index) => {
-        guessWord.splice(index, 1, e.key.toUpperCase());
-      });
-      word.textContent = guessWord.join("");
-      console.log(word.textContent, ranWord);
-    } else {
-      leftWord.textContent -= 1;
-
-      alert("Letter not found");
-    }
-    if (word.textContent === ranWord.toUpperCase()) {
-      alert("You got it right");
-      nextBtn.removeAttribute("style");
-    } else {
-      if (leftWord.textContent < 1) {
-        alert("Out of attempt");
-        retryBtn.removeAttribute("style");
-        nextBtn.setAttribute("style", "display: none");
-        return;
-      }
-    }
-  });
-  console.log(enteredLetter);
+  console.log("no of dash: ", word.textContent.length);
+  return word.textContent;
 }
 
 function randomNum(num) {
